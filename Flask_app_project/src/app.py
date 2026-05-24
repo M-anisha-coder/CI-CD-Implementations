f3b388002de3e176d2edb2b3fb724b6e99747611
M-anisha-coder <manishavisvkrama12345@gmail.com>
2026-05-24 21:36:12 +0530

    Move Flask app to src directory and update workflow

--- Flask_app_project/src/app.py
+++ Flask_app_project/src/app.py
@@ -7,3 +7,5 @@ def home():
     return render_template("travel-website.html")
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
+
--- Flask_app_project/test/test_app.py
+++ Flask_app_project/test/test_app.py
@@ -1,4 +1,4 @@
-from app import app
+from  Flask_app_project.src.app import app
 
 
 def test_home():
